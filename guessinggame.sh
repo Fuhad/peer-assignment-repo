bb93d0f43e39bc22ec45454f97372cca2c19eb1e
Fuhad <fuhad.e@hotmail.com>
2019-11-25 12:31:54 +0100

    created guessinggame program

---
+++ guessinggame.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+function guessinggame {
+
+echo "Hello Human!"
+
+echo " "
+
+echo "Welcome to the guessing game!"
+
+echo " "
+
+echo "Guess how many files are in this directory"
+
+read response
+
+for number in $response
+ do
+  if [[ $number -gt 3 ]] 
+   then
+     echo "Your guess is too high. Try Again!"
+  elif [[ $number -lt 3 ]]
+   then 
+     echo "Your guess is too low. Try Again!"
+  else
+     echo "That's right. Good job!"
+fi
+done
+}
